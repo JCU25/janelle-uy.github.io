eb0d399523f4933475c581f402aff03eb46dd777
jcu25 <jcuy@mcm.edu.ph>
2024-11-10 14:35:44 +0800

    feat: added react integration and added it to compilerOptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jcu25.github.io',
-  integrations: [tailwind()]
+  integrations: [
+    react(),
+    tailwind()
+  ]
 });
