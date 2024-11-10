9b56edc377bd341d25dce5843603146eea0195a2
jcu25 <jcuy@mcm.edu.ph>
2024-11-10 14:35:58 +0800

    feat: added more color into tailwind oclor library

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,11 @@ export default {
 				magenta: "#BB37DC",
 				purple: "#844BFF",
 				"navy-blue": "#065F91",
-				coral: "#FF7E7E"
+				coral: "#FF7E7E",
+				"dark-gray": "#454545",
+				black: "#181818",
+				"off-white": "#F6F6F6",
+				"light-gray": "#C0C0C0"
 			}
 		},
 	},
