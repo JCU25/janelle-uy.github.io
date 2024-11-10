e2c838dc08173d523b4433e65fc71333a357e3c8
jcu25 <jcuy@mcm.edu.ph>
2024-11-10 14:36:14 +0800

    feat: created top nav for mobile

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,34 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import React from "react";
+
+
+export default function MobileNav() {
+
+    return (
+        <Menu>
+            <MenuButton>
+                <Bars3Icon className="h-10"></Bars3Icon>
+            </MenuButton>
+
+            <MenuItems
+                transition
+                anchor="bottom end"
+                className="bg-gradient-to-r from-magenta to-navy-blue w-full flex flex-col text-white p-7 gap-4">
+                <MenuItem as="a" href="/">
+                    Home
+                </MenuItem>
+                <MenuItem as="a" href="/coding">
+                    Coding Projects
+                </MenuItem>
+                <MenuItem as="a" href="/">
+                    Designs
+                </MenuItem>
+                <MenuItem as="a" href="/about-me">
+                    BY THE WAY!!
+                </MenuItem>
+
+            </MenuItems>
+        </Menu>
+    )
+}
