0dcbe177d0cbebb4ea78f31b056b019e9be011bd
jcu25 <jcuy@mcm.edu.ph>
2024-11-24 22:23:58 +0800

    feat: added more data into projects API (incomplete)

--- src/pages/api/projects-[category].json.ts
+++ src/pages/api/projects-[category].json.ts
@@ -2,15 +2,58 @@ import type { APIContext } from "astro";
 
 const projects = {
     "coding": {
-        "Project OCP": {
-            description: "coding project",
-            img: "code img"
+        "OPED Website": {
+            description: "A website developed for the Office for Corporate Partnerships (OCP) of the Mapua Malayan Colleges Mindanao (MMCM) to track and monitor corporate partnerships and students internship data.",
+            img: "/assets/projects/oped.png",
+            details: [
+                // details shall be the "contributions" for the coding projects section
+                "In charge of server-side logic",
+                "Designed and implemented MySQL database",
+                "REST API development",
+                "Writing scripts to export information into downloadable excel files",
+                "Created complex database queries for all data (with multiple joined tables)",
+                "Participated in planning, and communicating with the administrators and staffs, and other teams and members to effectively carry out tasks, and implement new features on time.",
+                "Streamlined office operations by creating automated and scheduled tasks for email sending, contract expiration checking, etc.",
+                "Contributed to site deployment and maintenance with Vultr Object Storage, Linux VPS"
+            ],
+            role: [
+                "Lead Back End Developer (June 2022- August 2024)",
+                "Back End Developer (March 2022- June 2022)"
+            ],
+            tools: [
+                {
+                    name: "NodeJS",
+                    src: ""
+                },
+                {
+                    name: "MySQL",
+                    src: ""
+                },
+                {
+                    name: "ReactJS",
+                    src: ""
+                },
+                {
+                    name: "Ubuntu-based Virtual Private Server",
+                    src: ""
+                }
+            ]
         }
     },
     "design": {
         "Shelf Gleam": {
-            description: "design proj",
-            img: "design img"
+            description: "A website developed for the Office for Corporate Partnerships (OCP) of the Mapua Malayan Colleges Mindanao (MMCM) to track and monitor corporate partnerships and students internship data.",
+            img: "/assets/projects/oped.png",
+            details: [
+                // details shall be the "features list" for the coding projects section
+            ],
+            tools: [
+                {
+                    name: "Figma",
+                    src: "Figma"
+                }
+            ]
+
         }
     }
 }
